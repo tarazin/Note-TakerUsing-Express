be395f1c0c45b36052a58565f79c7826a347c09e
Zahra Azin <ztara.azin@gmail.com>
2020-08-05 20:29:37 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const fs = require("fs");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080
+
+// Sets up the Express app to handle data parsing
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+
+
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
